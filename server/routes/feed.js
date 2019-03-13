dbd7fd0024b350c93e813065b4a18ed838591f80
Georgi Velev <georgivelev4@gmail.com>
2019-03-13 19:26:02 +0200

    feed update

--- server/routes/feed.js
+++ server/routes/feed.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
 
 router.get('/courses', feedController.getCourses);
-router.post('/course/create', feedController.createCourse);
-router.post('/takecourse', feedController.takeCourse);
-router.post('/mycourses', feedController.myCourses);
-router.post('/deletecourse', feedController.deleteCourse);
-router.post('/editcourse', feedController.editCourse);
+router.post('/course/create',isAuth, feedController.createCourse);
+router.post('/takecourse',isAuth, feedController.takeCourse);
+router.post('/mycourses',isAuth, feedController.myCourses);
+router.post('/deletecourse',isAuth, feedController.deleteCourse);
+router.post('/editcourse',isAuth, feedController.editCourse);
 
 module.exports = router;
