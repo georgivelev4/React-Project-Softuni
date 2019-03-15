fd3d1b5d832167eb26a55fddadd935df787b693c
Georgi Velev <georgivelev4@gmail.com>
2019-03-15 22:33:30 +0200

    feedRoute update

--- server/routes/feed.js
+++ server/routes/feed.js
@@ -4,8 +4,8 @@ const isAuth = require('../middleware/is-auth');
 
 router.get('/courses', feedController.getCourses);
 router.post('/course/create',isAuth, feedController.createCourse);
-router.post('/takecourse', feedController.takeCourse);
-router.post('/mycourses', feedController.myCourses);
+router.post('/takecourse', isAuth, feedController.takeCourse);
+router.post('/mycourses', isAuth, feedController.myCourses);
 router.post('/deletecourse', isAuth, feedController.deleteCourse);
 router.post('/editcourse', isAuth, feedController.editCourse);
 
